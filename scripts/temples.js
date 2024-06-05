bbca62b82df40400934105b54a8a3c621027be6a
CWP <77127867+Camwp@users.noreply.github.com>
2024-06-05 15:58:47 -0700

    temple

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Set the current year in the footer
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+    // Set the last modified date in the footer
+    document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+});
+
+function toggleMenu() {
+    var headerContent = document.querySelector('.header-content');
+    headerContent.classList.toggle('responsive');
+}
