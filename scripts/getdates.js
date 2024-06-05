2c0866a9d8dd400461b4ffb4fd3e9e0ae91ef986
CWP <77127867+Camwp@users.noreply.github.com>
2024-06-05 12:54:29 -0700

    dates fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,11 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentYearSpan = document.getElementById("currentyear");
     const lastModifiedP = document.getElementById("lastModified");
 
-    // Set the current year
     const currentYear = new Date().getFullYear();
     currentYearSpan.textContent = currentYear;
 
-    // Set the last modified date
     const lastModifiedDate = document.lastModified;
     lastModifiedP.textContent = `Last Modified: ${lastModifiedDate}`;
 
